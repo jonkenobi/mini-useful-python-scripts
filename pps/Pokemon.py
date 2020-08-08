5c383ad2ca7f7a7babd695fbe2f54f2cc9b7aac0
jonkenobi <40736290+jonkenobi@users.noreply.github.com>
2020-08-08 13:48:33 +0900

    Some refactoring to Pokemon.py

--- pps/Pokemon.py
+++ pps/Pokemon.py
@@ -7,10 +7,9 @@ Created on Tue Apr 23 16:21:30 2019
 
 class Pokemon:
     def __init__(self,name):
-      self.lvl = 1
-      self.name = name
-    def get_level():
-        print(name +"is level"+lvl)
+        self.name = name
+    def get_name(self):
+        print("My name is " + self.name)
 class Charizard(Pokemon):
     def __init__(self):
         self.name = "Charizard"
@@ -18,19 +17,20 @@ class Charizard(Pokemon):
     
 class Mewtwo(Pokemon):
     def __init__(self):
-        self.moves = {"Psychic Beam":110}
         self.name = "Mewtwo"
+        self.moves = {"Psychic Beam":110}
+    
 def fight(Pokemon1,Pokemon2):
     Move1 = list(Pokemon1.moves.keys())[0]
     Move2 = list(Pokemon2.moves.keys())[0]
-    print(Pokemon1.name+" used "+list(Pokemon1.moves.keys())[0])
-    print(Pokemon2.name+" used "+list(Pokemon2.moves.keys())[0])
+    print(Pokemon1.name+" used "+ Move1)
+    print(Pokemon2.name+" used "+ Move2)
     result = Pokemon1.moves[Move1]-Pokemon2.moves[Move2]
     if result>0:
         print(Pokemon1.name+" wins")
     else:
         print(Pokemon2.name+ " wins")
         
-Hilmi= Charizard()
-Prince = Mewtwo()
-fight(Prince,Hilmi)
+player1 = Charizard()
+player2 = Mewtwo()
+fight(player1, player2)
